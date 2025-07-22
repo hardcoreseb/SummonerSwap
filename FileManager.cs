692a00ac4a30195558c56954a40581e6120c4008
hardcoreseb <sebastian.reinhold@yahoo.de>
2025-07-22 14:30:18 +0200

    Add project files.

---
+++ FileManager.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+
+namespace SummonerSwap.Helpers
+{
+    public static class FileManager
+    {
+        public static void CopyDirectory(string source, string destination)
+        {
+            foreach (string dirPath in Directory.GetDirectories(source, "*", SearchOption.AllDirectories))
+                Directory.CreateDirectory(dirPath.Replace(source, destination));
+
+            foreach (string filePath in Directory.GetFiles(source, "*.*", SearchOption.AllDirectories))
+                File.Copy(filePath, filePath.Replace(source, destination), true);
+        }
+    }
+}
---
+++ RiotClientService.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics;
+using System.IO;
+using System.Windows;
+
+namespace SummonerSwap.Services
+{
+    public static class RiotClientService
+    {
+        private static readonly string RiotClientExe = @"C:\Riot Games\Riot Client\RiotClientServices.exe";
+
+        public static void KillClient()
+        {
+            foreach (var proc in Process.GetProcessesByName("RiotClientServices"))
+            {
+                proc.Kill();
+                proc.WaitForExit();
+            }
+        }
+
+        public static void LaunchClient()
+        {
+            if (File.Exists(RiotClientExe))
+            {
+                Process.Start(RiotClientExe, "--launch-product=league_of_legends --launch-patchline=live --allow-multiple-clients");
+            }
+            else
+            {
+                MessageBox.Show("Riot Client not found at expected path.");
+            }
+        }
+    }
+}
